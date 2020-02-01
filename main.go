aade6df8cb666218ee2c74b9c0c410038f3395f1
j-hira <jun1513910@gmail.com>
2020-02-02 01:28:45 +0900

    anacondaを導入

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
-	"fmt"
-	_ "github.com/joho/godotenv/autoload"
+	"log"
 	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/ChimeraCoder/anaconda"
 )
 
+func getTwitterApi() *anaconda.TwitterApi {
+	anaconda.SetConsumerKey(os.Getenv("API_KEY"))
+	anaconda.SetConsumerSecret(os.Getenv("API_SECRET_KEY"))
+	return anaconda.NewTwitterApi(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
+}
+
 func main() {
-	apiKey := os.Getenv("API_KEY")
-	fmt.Println(apiKey)
+	api := getTwitterApi()
+	text := "初めての投稿"
+	tweet, err := api.PostTweet(text, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(tweet.Text)
 }
