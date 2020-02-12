3ab74404e6f6d7964b8fe56e259c13e6b2f043cb
j-hira <jun1513910@gmail.com>
2020-02-12 14:29:14 +0900

    現在時刻をTweet

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/j-hira/twitter_bot/twitter"
 
@@ -10,10 +11,21 @@ import (
 
 func main() {
 	api := twitter.GetApi()
-	text := "初めての投稿"
-	tweet, err := api.PostTweet(text, nil)
+	text := "現在時刻: "
+	tweet, err := api.PostTweet(text+getDate(), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println(tweet.Text)
+}
+
+func getDate() string {
+	t := time.Now()
+	loc, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		loc = time.FixedZone("Asia/Tokyo", 9*60*60)
+	}
+	t = t.In(loc)
+	date := t.Format("2006年01月02日 15時04分05秒")
+	return date
 }
